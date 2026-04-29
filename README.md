390dfcdb982b3cf4d39431920c3f240b121e6781
lutzdan <lutzdaniela05@gmail.com>
2026-04-28 17:01:01 -0700

    Empty Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# dyna_travel_lapaz
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dyna_travel_lapaz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dyna_travel_lapaz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
